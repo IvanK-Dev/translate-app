f6e12fbc8b576a5040ed74467b57154d95df3b80
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-11-28 17:02:08 +0200

    Deleted unneccessary dependency

--- server/shopify.js
+++ server/shopify.js
@@ -1,10 +1,8 @@
-import { BillingInterval } from "@shopify/shopify-api";
+import { BillingInterval, LATEST_API_VERSION } from "@shopify/shopify-api";
 import { shopifyApp } from "@shopify/shopify-app-express";
 import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
 import { restResources } from "@shopify/shopify-api/rest/admin/2023-10";
 import {join} from "path";
-import dotenv from "dotenv";
-dotenv.config();
 
 const DB_PATH = `${join(process.cwd(), 'database.sqlite')}`;
 
@@ -21,7 +19,7 @@ const billingConfig = {
 
 const shopify = shopifyApp({
   api: {
-    apiVersion: process.env.SHOPIFY_API_VERSION,
+    apiVersion: LATEST_API_VERSION,
     restResources,
     billing: undefined, // or replace with billingConfig above to enable example billing
   },
