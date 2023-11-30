4b0a51c771ccb7dc675d7a342abd9b77e7ccbfa8
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-11-30 13:19:57 +0200

    Resolved conflicts

--- web/index.jsx
+++ web/index.jsx
@@ -5,10 +5,7 @@ import { initI18n } from "./utils/i18nUtils.js";
 
 // Ensure that locales are loaded before rendering the app
 initI18n().then(() => {
-  const root = ReactDOM.createRoot(document.getElementById('app'));
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+  ReactDOM.createRoot(document.getElementById('app')).render(
+    <App />
   );
 });
