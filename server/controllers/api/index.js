f2034c754f73a92aeb51e4d9352563574747f76a
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-12-06 20:14:55 +0200

    Added translate register

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -1,6 +1,8 @@
 import contrsWrapper from "../../helpers/contrsWrapper.js";
 import getEntity from "./getEntity.js";
+import regTranslation from "./regTranslation.js";
 
 export default {
   getEntity: contrsWrapper(getEntity),
-}
+  regTranslation: contrsWrapper(regTranslation),
+};
